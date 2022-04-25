462003f94dfb9e9f268f5ada98097c6214e1012a
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2022-04-24 17:55:13 -0700

    Update loaders.ts

--- src/utils/loaders.ts
+++ src/utils/loaders.ts
@@ -2,7 +2,7 @@ import { glob } from "glob";
 
 export async function LoadFiles(dirs: string[]) {
 	for(const dir of dirs) {
-		const files = glob.sync(`**/${dir}/**/*.js`);
+		const files = glob.sync(`**/${dir}/**/*.ts`);
 		for(const file of files) {
 			await import(`../../${file}`);
 		}
